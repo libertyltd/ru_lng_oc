a5e29e74812cfc3f7c37a90b2129e1ef7e903759
libertyltd <roman-991@ya.ru>
2016-03-02 12:39:36 +0300

    translate information/*

--- information/contact.php
+++ information/contact.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']  = 'Contact Us';
+$_['heading_title']  = 'Связаться с нами';
 
 // Text
-$_['text_location']  = 'Our Location';
-$_['text_store']     = 'Our Stores';
-$_['text_contact']   = 'Contact Form';
-$_['text_address']   = 'Address';
-$_['text_telephone'] = 'Telephone';
-$_['text_fax']       = 'Fax';
-$_['text_open']      = 'Opening Times';
-$_['text_comment']   = 'Comments';
-$_['text_success']   = '<p>Your enquiry has been successfully sent to the store owner!</p>';
+$_['text_location']  = 'Наше расположение';
+$_['text_store']     = 'Наши магазины';
+$_['text_contact']   = 'Написать нам';
+$_['text_address']   = 'Адрес';
+$_['text_telephone'] = 'Телефон';
+$_['text_fax']       = 'Факс';
+$_['text_open']      = 'Режим работы';
+$_['text_comment']   = 'Комментарии';
+$_['text_success']   = '<p>Ваш запрос был успешно отправлен!</p>';
 
 // Entry
-$_['entry_name']     = 'Your Name';
-$_['entry_email']    = 'E-Mail Address';
-$_['entry_enquiry']  = 'Enquiry';
+$_['entry_name']     = 'Ваше имя';
+$_['entry_email']    = 'Ваш E-Mail';
+$_['entry_enquiry']  = 'Ваш запрос';
 
 // Email
-$_['email_subject']  = 'Enquiry %s';
+$_['email_subject']  = 'Запрос %s';
 
 // Errors
-$_['error_name']     = 'Name must be between 3 and 32 characters!';
-$_['error_email']    = 'E-Mail Address does not appear to be valid!';
-$_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters!';
+$_['error_name']     = 'Имя должно содержать от 1 до 32 символов!';
+$_['error_email']    = 'E-Mail введен неверно!';
+$_['error_enquiry']  = 'Длина текста запроса должна сожержать от 10 до 3000 символов!';
--- information/sitemap.php
+++ information/sitemap.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'Site Map';
+$_['heading_title']    = 'Карта сайта';
 
 // Text
-$_['text_special']     = 'Special Offers';
-$_['text_account']     = 'My Account';
-$_['text_edit']        = 'Account Information';
-$_['text_password']    = 'Password';
-$_['text_address']     = 'Address Book';
-$_['text_history']     = 'Order History';
-$_['text_download']    = 'Downloads';
-$_['text_cart']        = 'Shopping Cart';
-$_['text_checkout']    = 'Checkout';
-$_['text_search']      = 'Search';
-$_['text_information'] = 'Information';
-$_['text_contact']     = 'Contact Us';
+$_['text_special']     = 'Акции';
+$_['text_account']     = 'Личный кабинет';
+$_['text_edit']        = 'Личная информация';
+$_['text_password']    = 'Пароль';
+$_['text_address']     = 'Мои адреса';
+$_['text_history']     = 'История заказов';
+$_['text_download']    = 'Загрузки';
+$_['text_cart']        = 'Корзина покупок';
+$_['text_checkout']    = 'Оформление заказа';
+$_['text_search']      = 'Посик';
+$_['text_information'] = 'Информация';
+$_['text_contact']     = 'Написать нам';
